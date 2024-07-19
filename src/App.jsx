b70cdac162a78492bdf9319b497645b1e3a41fba
skanda-vasishta <124769936+skanda-vasishta@users.noreply.github.com>
2024-07-19 02:40:05 -0700

    why not

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+// // src/App.jsx
+// import React from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Navbar from './components/Navbar';
+// import Home from './components/Home';
+// import About from './components/About';
+// import NoPage from './components/NoPage';
+
+// function App() {
+//   return (
+//     <BrowserRouter basename={import.meta.env.BASE_URL}>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/home" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="*" element={<NoPage />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
 // src/App.jsx
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -8,7 +31,7 @@ import NoPage from './components/NoPage';
 
 function App() {
   return (
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -16,7 +39,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
-    </HashRouter>
+    </>
   );
 }
 
